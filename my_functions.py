4850183d02c4de58910947ab529bee8551bb62e9
Yuchen Ji <yuchenji99@gamil.com>
2024-01-12 00:32:26 -0500

    BO Experiment and Plot

--- my_functions.py
+++ my_functions.py
@@ -1,11 +1,9 @@
 # my_functions.py
-
 import os
 import sys
-from multiprocessing import Pool
 import json
-
 import numpy as np
+from multiprocessing import Pool
 from skopt import gp_minimize
 from skopt.space import Real
 from skopt.utils import use_named_args
@@ -13,13 +11,12 @@ from skopt.utils import use_named_args
 from bo_bidder import BOBidder
 from ad_auction import AdAuction
 
-# makes testing easier
+# Global parameters for ease of testing
 g_params = dict(
     num_iterations=1000,
     n_calls=84,
     num_reps=10,
 )
-
 
 def evaluate(ad_auction, num_iterations):
     trace = []
@@ -28,8 +25,6 @@ def evaluate(ad_auction, num_iterations):
         trace.append(r)
     return np.array(trace).mean()
 
-
-# Function to run a single replication in a separate process
 def run_single_replication(args):
     g_params, acq_func, i_replication = args
     ad_auction = AdAuction(
@@ -38,14 +33,12 @@ def run_single_replication(args):
         extra_classes={"BOBidder": BOBidder},
     )
 
-    # Define the parameter space
     num_params = 24
     param_space = [Real(-1.0, 1.0, name=f"param{i}") for i in range(num_params)]
-
     bo_bidder = ad_auction.us().bidder
 
-    # Objective function
     i_call = 0
+    trace = []
 
     @use_named_args(param_space)
     def objective(**params):
@@ -53,19 +46,11 @@ def run_single_replication(args):
         param_values = np.array(list(params.values()))
         bo_bidder.set_parameters(param_values)
         advantage = evaluate(ad_auction, num_iterations=g_params["num_iterations"])
-        print(
-            "OBJECTIVE:",
-            os.getpid(),
-            i_call,
-            g_params["num_iterations"],
-            g_params["n_calls"],
-            advantage,
-        )
-        sys.stdout.flush()
+        trace.append(advantage)
         i_call += 1
         return -advantage
 
-    result = gp_minimize(
+    gp_minimize(
         objective,
         param_space,
         n_calls=g_params["n_calls"],
@@ -74,38 +59,30 @@ def run_single_replication(args):
         verbose=True,
     )
 
-    y_total_replication = -sum(result.func_vals)
-
-    # Save results to a file
     output_path = f"results_{acq_func}_{i_replication}.json"
     with open(output_path, "w") as file:
-        json.dump(
-            {
-                "acq_func": acq_func,
-                "replication": i_replication,
-                "y_total": y_total_replication,
-            },
-            file,
-        )
+        json.dump({"acq_func": acq_func, "replication": i_replication, "trace": trace}, file)
 
     return output_path
 
+def run_all_experiments(acquisition_functions, start_rep, end_rep):
+    all_results = []
 
-def run_experiment(acq_func):
-    print(f"EXPERIMENT: {g_params}")
-    sys.stdout.flush()
+    for i_replication in range(start_rep, end_rep):
+        results = {}
+        for acq_func in acquisition_functions:
+            print(f"Running experiment for acquisition function: {acq_func} with replication {i_replication}")
+            sys.stdout.flush()
 
-    with Pool(g_params["num_reps"]) as p:
-        args = [(g_params, acq_func, i) for i in range(g_params["num_reps"])]
-        result_files = p.map(run_single_replication, args)
+            result_file = run_single_replication((g_params, acq_func, i_replication))
 
-    # Collect results from files
-    y_totals = []
-    for file_name in result_files:
-        with open(file_name, "r") as file:
-            data = json.load(file)
-            y_totals.append(data["y_total"])
-            os.remove(file_name)
+            with open(result_file, "r") as file:
+                data = json.load(file)
+                trace = data["trace"]
+                cumulative_trace = np.cumsum(trace)
+                results[acq_func] = cumulative_trace
+                os.remove(result_file)
 
-    avg_total_advantage = np.mean(y_totals)
-    return avg_total_advantage
+        all_results.append(results)
+
+    return all_results
